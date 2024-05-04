a2383bccd11f1dda231fabe5985838e12e03e85f
vibhor <vibgitcode27@gmail.com>
2024-05-05 03:49:10 +0530

    basic openAPI spec file made with hono

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import { createRoute } from "@hono/zod-openapi";
+import { ParamsSchema, ReqDescription } from "../inputs";
+import { UserSchema, ResDescriptionSchema } from "../outputs";
+import { OpenAPIHono } from "@hono/zod-openapi";
+
+const getUserRoute = createRoute({
+  method: "get",
+  path: "/user/{id}",
+  request: {
+    params: ParamsSchema,
+  },
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: UserSchema,
+        },
+      },
+      description: "This is a getuser route",
+    },
+  },
+});
+
+const getUserDescription = createRoute({
+  method: "get",
+  path: "/user/{id}",
+  request: {
+    params: ReqDescription,
+  },
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: ResDescriptionSchema,
+        },
+      },
+      description: "This is a getuser route",
+    },
+  },
+});
+
+const app = new OpenAPIHono();
+
+app.openapi(getUserRoute, (c) => {
+  const { id } = c.req.valid("param");
+  return c.json({
+    id,
+    age: 20,
+    name: "Vibhor Phalke",
+  });
+});
+
+app.openapi(getUserDescription, (c) => {
+  const { id } = c.req.valid("param");
+  return c.json({
+    id,
+    age: 20,
+    name: "Vibhor Phalke",
+    description: "This is gonna be the best day of my life ok ???",
+  });
+});
+
+export default app;
